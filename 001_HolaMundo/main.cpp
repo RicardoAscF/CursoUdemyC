6765dbd1255b86d2348ce4de71f4fbf3adf3d73d
RicardoAscF <ricardoascencio3.14@gmail.com>
2023-09-03 04:41:53 -0600

    Hola Mundo

--- 001_HolaMundo/main.cpp
+++ 001_HolaMundo/main.cpp
@@ -1,24 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
- */
-
-/* 
- * File:   main.cpp
- * Author: rick
- *
- * Created on 3 de septiembre de 2023, 4:11
- */
-
-#include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+int main(){
 
-    return 0;
+    cout<<"Hola MUndo :D";
+return 0;
 }
+
 
