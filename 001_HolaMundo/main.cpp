288d0e835fd581b3430190c1bec641bbca7a5977
RicardoAscF <ricardoascencio3.14@gmail.com>
2023-09-03 04:16:54 -0600

    First Commit

---
+++ 001_HolaMundo/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rick
+ *
+ * Created on 3 de septiembre de 2023, 4:11
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
