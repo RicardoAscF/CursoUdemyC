1476d5544978adf93e97ce3545497d6a308ba702
RicardoAscF <ricardoascencio3.14@gmail.com>
2023-09-03 23:08:17 -0600

    Operaciones Aritmeticas

---
+++ 002_operacionesAritmeticas/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float num1,num2;
+    cout<<"NUmero 1:"<<endl;
+    cin>>num1;
+    cout<<"NUmero 2:"<<endl;
+    cin>>num2;
+    
+    cout<<num1<<"+"<<num2<<"="<<num1+num2<<endl;
+    cout<<num1<<"-"<<num2<<"="<<num1-num2<<endl;
+    cout<<num1<<"*"<<num2<<"="<<num1*num2<<endl;
+    cout<<num1<<"/"<<num2<<"="<<num1/num2<<endl;
+    
+    
+    return 0;
+}
