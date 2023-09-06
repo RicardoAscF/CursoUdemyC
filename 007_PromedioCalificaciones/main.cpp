9a4a5f9946ba9c27a75bdc851e6609c3e21a3a67
RicardoAscF <ricardoascencio3.14@gmail.com>
2023-09-06 00:38:18 -0600

    Promedio Calificaciones

---
+++ 007_PromedioCalificaciones/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int calificacion,suma=0;
+    cout<<"Calificacion del alumno 1: ";
+    cin>>calificacion;
+    suma+=calificacion;
+    
+    
+    cout<<"Calificacion del alumno 2: ";
+    cin>>calificacion;
+    suma+=calificacion;
+  
+    
+    cout<<"Calificacion del alumno 3: ";
+    cin>>calificacion;
+    suma+=calificacion;
+   
+    
+    cout<<"Calificacion del alumno 4: ";
+    cin>>calificacion;
+    suma+=calificacion;
+    
+    cout<<"El promedio de las calificaciones es: "<<suma/4;
+            
+            
+            
+            
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
